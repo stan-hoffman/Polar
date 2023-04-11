4c641632a336f5865aed2134a1d6dbc1f6a90eac
stanislavodnorukov <odnorukov.stanislav@gmail.com>
2023-04-11 16:15:11 -0400

    add new file jquerry validate in js folder+ add link to html

--- src/js/script.js
+++ src/js/script.js
@@ -37,6 +37,21 @@ $(document).ready(function(){
   
       toggleSlide('.catalog-item__link');
       toggleSlide('.catalog-item__back');
+
+
+      //Modals
+      $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+      });
+      $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('fast');
+      });
+      $('.button_mini').each(function(i) {
+        $(this).on('click', function () {
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('slow');
+        });
+      });
   });
 
 
